39c4d1e12fa0cb58ed38b7558b12f57545869550
Samuel Giorgievski <samgiorgievski@gmail.com>
2022-11-23 14:32:37 -0500

    Added connect message and send name data to server

--- client.js
+++ client.js
@@ -1,13 +1,17 @@
 const net = require("net");
 
 const connect = function () {
-  const conn = net.createConnection({
+  const conn = net.createConnection({ //establish connection
     host: "165.227.47.243",
     port: "50541",
   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+  conn.setEncoding("utf8"); // interpret incoming data as text
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: Sam");
+  });
 
   conn.on("data", (data) => {
     console.log(data);
